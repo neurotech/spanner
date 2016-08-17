764b6a0ed1509a4e748c8e042dcf037c0d447f26
Tim Douglas <neurotech@gmail.com>
2016-08-18 09:25:59 +1000

    Updated issues to get count, group by category

--- app/components/issues/index.js
+++ app/components/issues/index.js
@@ -1,16 +1,27 @@
 'use strict';
 const path = require('path');
 const pug = require('pug');
+const _ = require('underscore');
 
 module.exports = {
   template: pug.renderFile(path.join(__dirname, 'issues.pug')),
   mixins: [require('../../mixins/fetch-issues')],
   data: function () {
     return {
-      issues: []
+      issueCount: 0,
+      issues: [],
+      groupedIssues: {}
     };
   },
   created: function () {
     this.fetchAllIssues();
+    this.fetchIssueCount();
+    this.$watch('issues', function (newVal, oldVal) {
+      var grouped = _.groupBy(newVal, function (list) {
+        var chunk = list.category;
+        return chunk;
+      });
+      this.groupedIssues = grouped;
+    });
   }
 };
