c476c0dd06a10025f8ba59bddfac92a0c3ff0b1d
Tim Douglas <neurotech@gmail.com>
2016-08-19 14:37:51 +1000

    Added dispatch method to monitor freshness

--- app/lib/fetch.js
+++ app/lib/fetch.js
@@ -15,6 +15,9 @@ fetch.edumate = function (endpoint, options, key) {
   this.$set(key, []);
   return got(endpoint, options)
     .then(response => {
+      if (response.body.length > 0) {
+        this.$dispatch('freshness-update', response.body[0].freshness);
+      }
       this.$set(key, response.body);
     })
     .catch(error => {
