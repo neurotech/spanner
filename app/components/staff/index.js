4ec55730bdd66d0bd80f21abc9958dee9b7873f7
Tim Douglas <neurotech@gmail.com>
2016-08-19 14:52:06 +1000

    Added staffAwayDetails variable, fader support

--- app/components/staff/index.js
+++ app/components/staff/index.js
@@ -7,11 +7,15 @@ module.exports = {
   mixins: [require('../../mixins/fetch-staff-away')],
   data: function () {
     return {
+      staffAwayDetails: [],
       staffAway: {
         now: null,
         soon: null,
         allDay: null
       }
     };
+  },
+  transitions: {
+    'fader': require(path.join(__dirname, '../../transitions/fader'))
   }
 };
