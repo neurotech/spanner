2bc6f968696720c704e3c1ea86604fb0535f7a2c
Tim Douglas <neurotech@gmail.com>
2016-08-22 22:06:13 +1000

    Added clipboard support to issues, minutely data fetch

--- app/components/issues/index.js
+++ app/components/issues/index.js
@@ -2,19 +2,24 @@
 const path = require('path');
 const pug = require('pug');
 const _ = require('underscore');
+const config = require('../../config');
 
 module.exports = {
   template: pug.renderFile(path.join(__dirname, 'issues.pug')),
-  mixins: [require('../../mixins/fetch-issues')],
+  mixins: [
+    require('../../mixins/fetch-issues'),
+    require('../../mixins/clipboard')
+  ],
   data: function () {
     return {
       issueCount: 0,
       issues: [],
-      groupedIssues: {}
+      groupedIssues: {},
+      clipboardStatus: ''
     };
   },
   created: function () {
-    this.fetchAllData();
+    setInterval(this.fetchAllData(), config.get('cycles.everyMinute'));
     this.fetchIssueCount();
     this.$watch('issues', function (newVal, oldVal) {
       var grouped = _.groupBy(newVal, function (list) {
